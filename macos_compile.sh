a07b277cb5687d5736948528f77009f5ddc8b87c
Carl-Y <52552476+HH45137@users.noreply.github.com>
2024-02-27 20:04:45 +0800

    Update

---
+++ macos_compile.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+VULKAN_SDK=~/VulkanSDK/1.3.268.1/macOS/
+
+cd ./Shaders/
+
+if [[ ! -x "./bintoc" ]]
+then
+    gcc bintoc.c -o bintoc
+fi
+
+
+for file in *.vert; do $VULKAN_SDK/bin/glslangValidator -V ${file} -o "./Compiled/${file%.*}.vspv"; done
+
+for file in *.frag; do $VULKAN_SDK/bin/glslangValidator -V ${file} -o "./Compiled/${file%.*}.fspv"; done
+
+for file in *.comp; 
+do
+    filename=${file}
+    substring="sops"    
+    if test "${filename#*$substring}" != "$filename"; then
+        $VULKAN_SDK/bin/glslangValidator -V ${file} --target-env vulkan1.3 -o "./Compiled/${file%.*}.cspv"; 
+    else
+        $VULKAN_SDK/bin/glslangValidator -V ${file} -o "./Compiled/${file%.*}.cspv"; 
+    fi
+done
+
+
+for file in ./Compiled/*.vspv; do ./bintoc ${file} `basename ${file%.*}`_vert_spv ${file%.*}_vert.c; done
+
+for file in ./Compiled/*.fspv; do ./bintoc ${file} `basename ${file%.*}`_frag_spv ${file%.*}_frag.c; done
+
+for file in ./Compiled/*.cspv; do ./bintoc ${file} `basename ${file%.*}`_comp_spv ${file%.*}_comp.c; done
+
+
+cd ..
